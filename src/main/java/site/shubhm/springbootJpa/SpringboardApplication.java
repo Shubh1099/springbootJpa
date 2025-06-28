07c62f37b64daa2b45d03bab5fee3c30c39e5ed0
shubh <shubhammane434@gmail.com>
2025-06-29 01:16:24 +0530

    modify

--- src/main/java/site/shubhm/springbootJpa/SpringboardApplication.java
+++ src/main/java/site/shubhm/springbootJpa/SpringboardApplication.java
@@ -3,8 +3,10 @@ package site.shubhm.springbootJpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import site.shubhm.springbootJpa.entities.Student;
 import site.shubhm.springbootJpa.services.StudentService;
 import site.shubhm.springbootJpa.services.StudentServiceImpl;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringboardApplication {
@@ -13,62 +15,62 @@ public class SpringboardApplication {
 		ApplicationContext context = SpringApplication.run(SpringboardApplication.class, args);
 		StudentService studentService = context.getBean(StudentServiceImpl.class);
 
-		// INSERT
+//				INSERT
 
-//		Student std = new Student();
-//		std.setName("vaibhav");
-//		std.setCity("nwnw");
-//		std.setEmail("mvs@mail.com");
-//		std.setGender("Male");
-//		std.setPassword("asdqwerr");
-//
-//		boolean status = studentService.addStudentDetails(std);
-//
-//		if(status){
-//			System.out.println("Record has been added successfully!");
-//		}
-//		else{
-//			System.out.println("Record not inserted.");
-//		}
+		Student std = new Student();
+		std.setName("vaibhav");
+		std.setCity("nwnw");
+		std.setEmail("mvs@mail.com");
+		std.setGender("Male");
+		std.setPassword("asdqwerr");
+
+		boolean status = studentService.addStudentDetails(std);
+
+		if(status){
+			System.out.println("Record has been added successfully!");
+		}
+		else{
+			System.out.println("Record not inserted.");
+		}
 
 		// SELECT ALL
-//		List<Student> stdList= studentService.getAllStudentDetails();
-//
-//		for (Student std1:stdList){
-//			System.out.println("Id :" + std1.getId());
-//			System.out.println("Name :" + std1.getName());
-//			System.out.println("Email :" + std1.getEmail());
-//			System.out.println("Password :" + std1.getPassword());
-//			System.out.println("Gender :" + std1.getGender());
-//			System.out.println("City :" + std1.getCity());
-//
-//		}
+		List<Student> stdList= studentService.getAllStudentDetails();
+
+		for (Student std1:stdList){
+			System.out.println("Id :" + std1.getId());
+			System.out.println("Name :" + std1.getName());
+			System.out.println("Email :" + std1.getEmail());
+			System.out.println("Password :" + std1.getPassword());
+			System.out.println("Gender :" + std1.getGender());
+			System.out.println("City :" + std1.getCity());
+
+		}
 
 		//  SELECT BY ID
 
 
-//		Student std2 = studentService.getStudentById(21L);
-//
-//		if(std2!=null){
-//			System.out.println("Details ::________________ "+std2.getName()+" " + std2.getEmail() + " " + std2.getGender());
-//		}
-//
-//		else{
-//			System.out.println("Student not found!");
-//		}
+		Student std2 = studentService.getStudentById(21L);
+
+		if(std2!=null){
+			System.out.println("Details ::________________ "+std2.getName()+" " + std2.getEmail() + " " + std2.getGender());
+		}
+
+		else{
+			System.out.println("Student not found!");
+		}
 
 
 
 		// UPDATE
 
-//		boolean updateStatus = studentService.updateDetails(1L,"asdljkhdfkjb");
-//
-//		if (updateStatus){
-//			System.out.println("Record updated successfully!");
-//		}
-//		else{
-//			System.out.println("error occured!");
-//		}
+		boolean updateStatus = studentService.updateDetails(1L,"asdljkhdfkjb");
+
+		if (updateStatus){
+			System.out.println("Record updated successfully!");
+		}
+		else{
+			System.out.println("error occured!");
+		}
 
 
 
